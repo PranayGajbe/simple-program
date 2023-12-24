39781fcf5ba7b1e42bb3a925ecfc1d1bce81b923
Pranay Ashok  Gajbe <100502178+PranayGajbe@users.noreply.github.com>
2023-12-24 12:51:22 +0530

    Add files via upload

---
+++ factorial_number.java
@@ -0,0 +1,21 @@
+class factorial_number{
+     public static void main(String args[]){
+     int fact=8;
+       for(int i=1;i<=10;i++){
+       if(fact%i==0){
+          System.out.println(i);
+
+
+       }
+
+
+       }
+
+
+
+
+    }
+
+
+
+}
